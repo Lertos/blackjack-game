5f9298c3f2925c8afd48ee65c260fdf72d09eed2
Lertos <deetasticgaming@gmail.com>
2022-11-05 21:32:54 -0300

    Finishing bank class

--- src/Bank.java
+++ src/Bank.java
@@ -1,3 +1,24 @@
 public class Bank {
 
+    private int balance;
+    public Bank(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void deposit(int cash) {
+        this.balance += cash;
+    }
+
+    public boolean withdraw(int cash) {
+        if (cash >= balance) {
+            this.balance -= cash;
+            return true;
+        }
+        return false;
+    }
+
 }
