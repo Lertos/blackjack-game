0ee61b2be60f3a0773880828995f5d2eebbd7a56
Lertos <deetasticgaming@gmail.com>
2022-11-06 20:18:16 -0400

    Adding fields and constructor

--- src/House.java
+++ src/House.java
@@ -1,2 +1,10 @@
 public class House {
+
+    private Bank bank;
+    private Hand hand;
+
+    public House(int startingCash) {
+        this.bank = new Bank(startingCash);
+        this.hand = new Hand();
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,15 @@
 public class Player {
+
+    private Bank bank;
+    private Hand hand;
+    private String name;
+
+    public Player(String name, int startingCash) {
+        this.bank = new Bank(startingCash);
+        this.hand = new Hand();
+        this.name = name;
+    }
+
+
+
 }
