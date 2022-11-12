697ed20df7cfacbb66fca35e64536f4fd7012b88
Lertos <deetasticgaming@gmail.com>
2022-11-11 22:15:48 -0400

    Adding a setup method

--- src/DeckTest.java
+++ src/DeckTest.java
@@ -1,21 +1,25 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class DeckTest {
+
+    private Deck deck;
+
+    @Before
+    public void setup() {
+        deck = new Deck();
+        deck.shuffleCards();
+    }
+
     @Test
     public void shuffleCards() {
-        Deck deck = new Deck();
-        deck.shuffleCards();
-
         assertEquals(52, deck.getAllCards().size());
     }
 
     @Test
     public void getRandomCard() {
-        Deck deck = new Deck();
-        deck.shuffleCards();
-
         assertNotEquals(deck.getRandomCard(), deck.getRandomCard());
     }
 }
